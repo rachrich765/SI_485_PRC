c0d83ae609075ba7343fca8f845ac08b43831df1
rachrich765 <rachjr@umich.edu>
2019-02-18 14:50:27 -0500

    Add files via upload

--- rachel/US_Dept_Health_Human_Services.py
+++ rachel/US_Dept_Health_Human_Services.py
@@ -6,11 +6,15 @@ import requests
 import textract
 from bs4 import BeautifulSoup
 import lxml.html as lh
+import csv
 
 
 
 url = 'https://ocrportal.hhs.gov/ocr/breach/breach_report.jsf'
+#find table on page using pandas
 tables = pd.read_html(url)
+#get table with desired data
 data_USDHHSOCR = tables[1]
+#write pandas table to CSV file
+data_USDHHSOCR.to_csv(path_or_buf='USDHHSOCR_data.csv')
 print("--- %s seconds ---" % (time.time() - start_time))  
-print(data_USDHHSOCR.head())
