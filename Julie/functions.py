0f68bcd6f402557b5f8a59472fc430fbef545af2
julieburke <jfburke@umich.edu>
2019-02-23 13:51:55 -0500

    merge oregon and wisconsin in main

--- Julie/functions.py
+++ Julie/functions.py
@@ -33,23 +33,70 @@ def update_Oregon(most_recent_breach):
     for x in s('table', {'class':"webgrid-table", 'id':"grid"}):
         for item in x('a'):
             more_info_urls.append('https://justice.oregon.gov' + item.get('href'))
+
+    breaches_info = {'Organization Name': [], 'Reported Date': [],
+                    'Date(s) of Breach':[], 'Date(s) of Discovery of Breach': [],
+                    'Notice Provided to Consumers':[]}
     
-    breaches_info = []
-    most_recent_breach = more_info_urls[0]
     
     for url in more_info_urls:
-        while url != most_recent_breach:
+        if url != most_recent_breach:
             try:
-                temp = pd.read_html(url)[0].set_index(0).to_dict()[1]
-                breaches_info.append(temp)#more_info = pd.concat([more_info, temp])
+                temp_dict = {}
+                s2 = basic_beautiful_soup(url)
+                l = []
+                for x in s2('td'):
+                    l.append(x.text.strip(':').strip('\r').strip('\n'))
+                for item in l[::2]:
+                    temp_dict[item] = l[l.index(item)+1]
+                for item in temp_dict:
+                    breaches_info[item].append(temp_dict[item])
+
             except:
                 pass
-    
+        #else:
+        #    break 
+    most_recent_breach = more_info_urls[0]
     more_info = pd.DataFrame(breaches_info)
 
-    more_info = more_info.drop(columns = ['Reported Date:', 'Date(s) of Breach:'])
-    more_info.columns = ['Date(s) of Discovery of Breach', 'Notice Provided to Consumers' ,'Organization']
-
-    df = pd.merge(oregon, more_info)
+    more_info = more_info.drop(columns = ['Reported Date', 'Date(s) of Breach'])
+    more_info = more_info.rename(index=str, columns={"Organization Name": "Organization"})
+    #more_info.columns = ['Date(s) of Discovery of Breach', 'Notice Provided to Consumers' ,'Organization']
     
+    df = pd.merge(oregon, more_info)
+    df = df.rename(index=str, columns={"Organization": "Name of Entity ", 'Notice Provided to Consumers': 'Date Notice Provided to Consumers'})
+    df['State Reported'] = 'Oregon'
     return df, most_recent_breach
+
+
+def update_Wisconsin(most_recent_breach):
+
+    current_url = 'https://datcp.wi.gov/Pages/Programs_Services/DataBreaches.aspx'
+    temp = pd.read_html(current_url)
+    rows_list = []
+    for breach in temp:
+        row = {'Notice Provided to Consumers':breach.iloc[1][0],
+               'Dates of Breach':breach.iloc[1][1],
+               'Name of Entity':breach.iloc[1][2],
+               'Data Stolen':breach.iloc[1][3],
+               "Who's Affected":breach.iloc[3][0],
+               'Details':breach.iloc[3][1]}
+        rows_list.append(row)
+    wisconsin_now = pd.DataFrame(rows_list)
+
+    pre_current_year_url = 'https://datcp.wi.gov/Pages/Programs_Services/DataBreachArchive.aspx'
+    temp = pd.read_html(pre_current_year_url)
+    rows_list = []
+    for breach in temp:
+        row = {'Notice Provided to Consumers':breach.iloc[1][0],
+               'Dates of Breach':breach.iloc[1][1],
+               'Name of Entity':breach.iloc[1][2],
+               'Data Stolen':breach.iloc[1][3], 
+               "Who's Affected":breach.iloc[3][0],
+               'Details':breach.iloc[3][1]}
+        rows_list.append(row)
+    wisconsin_archive = pd.DataFrame(rows_list)
+
+    wisconsin = pd.concat([wisconsin_now, wisconsin_archive], ignore_index = True)
+    wisconsin['State Reported'] = 'Wisconsin'
+    return (wisconsin, 'xyz')
--- Julie/main.py
+++ Julie/main.py
@@ -2,12 +2,22 @@ from functions import *
 
 recent = pd.read_csv('recent.csv').set_index('state')
 #data_breach_chronology = pd.read_csv('data_breach_chronology.csv')
-data_breach_chronology = pd.DataFrame()
+
+columns = ['Name of Entity', 'Dates of Breach', 'Reported Date',
+       'Date(s) of Discovery of Breach', 'Date Notice Provided to Consumers','Data Stolen', "Who's Affected", 'Details', 'State Reported']
+data_breach_chronology = pd.DataFrame(columns=columns)
+
 #Oregon
 or_df, or_recent = update_Oregon(recent['recent']['Oregon'])
 recent['recent']['Oregon'] = or_recent
-data_breach_chronology.append(or_df)
+data_breach_chronology = data_breach_chronology.append(or_df)
 
+
+wi_df, wi_recent = update_Wisconsin(recent['recent']['Wisconsin'])
+recent['recent']['Wisconsin'] = wi_recent
+data_breach_chronology = data_breach_chronology.append(wi_df)
+
+print (data_breach_chronology.head())
 
 recent.to_csv('recent.csv')
 data_breach_chronology.to_csv('data_breach_chronology.csv')
