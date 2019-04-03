43901f89848d0af9041c4e1d8c04b8939ba537df
rachrich765 <rachjr@umich.edu>
2019-04-02 21:22:55 -0400

    Add files via upload

---
+++ rachel/NH_v2.py
@@ -0,0 +1,64 @@
+import time
+start_time = time.time()
+
+import pandas as pd
+import requests
+import textract
+from bs4 import BeautifulSoup
+import lxml.html as lh
+import urllib
+import PyPDF2 
+import os
+import csv
+import datefinder
+from dateutil.parser import parse
+
+dates = list()
+url = 'https://www.doj.nh.gov/consumer/security-breaches/'
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
+all_p = (soup.find_all('p'))
+letter_links = list()
+for p in all_p:
+	letter_links_a = (p.find_all('a', href=True))
+	if letter_links_a:
+		for x in letter_links_a:
+			letter_links.append(x['href'])
+titles = list()
+NH_dict = dict()
+links = list()
+for x in letter_links:
+	url2 = url + x
+	res2 = requests.get(url2)
+	soup2 = BeautifulSoup(res2.content, 'html.parser')
+	a = soup2.find_all('a', href=True)
+	for a2 in a:
+		text = a2.text
+		date = text.split(",",1)[-1]
+		try:
+			date2 = parse(text, fuzzy=True)
+			dates.append(date2)
+		except:
+			continue
+		link = str(a2['href'])
+		if link.endswith('pdf'):
+			if link not in links:
+				links.append('https://www.doj.nh.gov/consumer/security-breaches/' + link)
+NH_dict = dict()
+print(links)
+for l in links:
+	response = requests.get(l)
+	my_raw_data = response.content
+	with open("new_pdf.pdf", 'wb') as my_data:
+		my_data.write(my_raw_data)
+		my_data.close()
+	file = textract.process('new_pdf.pdf', method='pdfminer')
+	NH_dict[l] = file
+	os.remove("new_pdf.pdf")
+
+
+end_time = time.time()
+minutes = (end_time - start_time)/60
+print("minutes:", minutes)
+
+
